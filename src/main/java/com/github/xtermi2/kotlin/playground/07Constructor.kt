c06c257a06c76b876c0ba4cd23b12e182139be0d
Andreas Keefer <andreas.keefer@senacor.com>
2019-04-25 20:34:51 +0200

    more constructor

--- src/main/java/com/github/xtermi2/kotlin/playground/07Constructor.kt
+++ src/main/java/com/github/xtermi2/kotlin/playground/07Constructor.kt
@@ -4,21 +4,29 @@ package com.github.xtermi2.kotlin.playground
  * @author Andreas Keefer
  */
 fun main() {
-    println(User("foo").name)
-    println(UserVerbose("verbose").name)
+    val user = User("foo")
+    println("${user.name} is admin ${user.admin}")
+
+    val userVerbose = UserVerbose("verbose")
+    println("${userVerbose.name} is admin ${userVerbose.admin}")
 }
 
-class User(val name: String) {
+class User(val name: String, val admin: Boolean = false) {
 
 }
 
-// same as User, but verbose
-class UserVerbose(name: String) {
+// same as User, but verbose with primary constructor
+class UserVerbose constructor(name: String, admin: Boolean) {
 
     val name: String
+    val admin: Boolean
 
+    // the init block is something like the constructor block in java
     init {
         this.name = name
+        this.admin = admin
     }
 
+    // a secondary constructor have to call the primary constructor
+    constructor(name: String) : this(name, false)
 }

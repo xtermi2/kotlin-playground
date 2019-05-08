960623a2fb8d2b235a6bf745242ea5e415115294
Andreas Keefer <andreas.keefer@senacor.com>
2019-05-08 18:01:43 +0200

    typo

--- src/main/java/com/github/xtermi2/kotlin/playground/01HelloWorld.kt
+++ src/main/java/com/github/xtermi2/kotlin/playground/01HelloWorld.kt
@@ -4,5 +4,5 @@ package com.github.xtermi2.kotlin.playground
  * @author Andreas Keefer
  */
 fun main() {
-    println("Hello WOrld") // no need for ;
+    println("Hello World") // no need for ;
 }
